846ac3b07e452f1e94f8bc0bc2f9e4c25dd8922c
homemovie <153278990+omidomidvari@users.noreply.github.com>
2026-03-16 10:11:54 -0700

    Create main.ts
    main marks everything that is happening

---
+++ src/mark/main.ts
@@ -0,0 +1,5 @@
+
+const fs = require('fs');
+const writeFileSync = require('fs/promises');
+const writeFileSync = require('fs/promises');
+fs.writeFileSync(join(__./src, 'logs'), data, { flag: 'w' });
